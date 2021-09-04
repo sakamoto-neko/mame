f3190dff7d2bc46d4a3c3b0fe000c49c017c5eb9
987123879113 <63495610+987123879113@users.noreply.github.com>
2021-09-04 17:57:11 +0900

    Adjust volume on Sys573 to reduce clipping

--- src/mame/machine/k573dio.cpp
+++ src/mame/machine/k573dio.cpp
@@ -166,8 +166,8 @@ void k573dio_device::device_add_mconfig(machine_config &config)
 {
 	KONAMI_573_DIGITAL_FPGA(config, k573fpga);
 	k573fpga->set_ram(ram);
-	k573fpga->add_route(0, ":lspeaker", 1.0);
-	k573fpga->add_route(1, ":rspeaker", 1.0);
+	k573fpga->add_route(0, ":lspeaker", 0.75);
+	k573fpga->add_route(1, ":rspeaker", 0.75);
 
 	DS2401(config, digital_id);
 }
